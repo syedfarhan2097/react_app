7bb71bd6c0bebe44748a9f36dacdd673874c5736
Syed Farhan <larka2097@gmil.com>
2022-10-04 18:44:05 +0500

    update 4 oct 6:44pm

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,13 +1,13 @@
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItems.css";
 import Card from "./Card";
-const ExpenseItems=(props)=> {
-    // let expenseDate= new Date(2022, 7, 31);
-    // let expenseName= 'Car Insurance';
-    // let expenseAmount = 251;
-    // const {
-    //   Name, Amount
-    // }=props
+const ExpenseItems = (props) => {
+  // let expenseDate= new Date(2022, 7, 31);
+  // let expenseName= 'Car Insurance';
+  // let expenseAmount = 251;
+  // const {
+  //   Name, Amount
+  // }=props
 
   return (
     <Card className="expense-item">
@@ -18,5 +18,5 @@ const ExpenseItems=(props)=> {
       </div>
     </Card>
   );
-}
+};
 export default ExpenseItems;
